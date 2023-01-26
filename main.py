a062402124de88817d4009db0b256bac6cba582f
Alex Neiman <42302007+neiman3@users.noreply.github.com>
2023-01-25 18:08:56 -0800

    Comments

--- main.py
+++ main.py
@@ -4,25 +4,38 @@ from text_maniupularion import clean_text, print_same_line, estimated_time_compl
 
 if __name__ == "__main__":
     # prompt user for CSV file names- one to read from and one to write to. Can be
-    # (file_in, file_out) = filenames()
-    file_in, file_out = './examples/input.csv', './examples/output.csv'
+    (file_in, file_out) = filenames()
+
     # parse the file to get a list of parts
     print("\nLoading your file...")
     parts = get_part_numbers_from_csv(file_in)
+
+    # some interface and formatting
     j = len(parts)
     print("Found {} parts in file.".format(j))
     print(" ")
+
+    # look up each part
     res = []
     for i in range(len(parts)):
         part = parts[i]
         print_same_line("Downloading data from AllDatasheet ({}/{}, {} remaining)".format(i+1,j,estimated_time_completion(i,j,avg_time=1)))
+        # make the request and get the HTML content
         html = get_html_from_alldatasheet(part)
+        # Parse the results table from the DOM
         main_table = get_item_table(html)
+        # extract the data
         descriptions = get_part_descriptions(main_table)
         url = get_datasheet_link(main_table)
+        # Make a CSV formatted line with the format:
+        # [part number], [url], [description 1], [description 2], [description 3]
         res.append(",".join([clean_text(part), url] + descriptions))
+
+    # interface
     print_same_line("Downloaded {} part details.".format(j))
     print("Saving to {}...".format(file_out))
+
+    # write to file
     f = open(file_out, 'w')
     for line in res:
         f.write(line)
